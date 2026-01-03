4f82d3e4b581cbe6601f51c4e8766aae76710ff3
Vicky8484 <viok0001@stud.ek.dk>
2026-01-03 09:38:00 +0100

    all done for now probably forever

--- main.js
+++
@@ -1,7 +0,0 @@
-document.getElementById("img").src = "img/billed1";
-document.getElementById("change").addEventListener("click", change);
-
-function change() {
-  let billed = document.getElementById("image");
-  billed.src = "img/billed2";
-}
